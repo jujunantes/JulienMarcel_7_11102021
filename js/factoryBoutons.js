540c836151b7a1ac969cda75e1ae87e9772b3874
Julien MARCEL <jul_marcel@hotmail.com>
2021-10-26 18:23:23 +0200

    Récupération ingrédients, ustensiles et appareils

---
+++ js/factoryBoutons.js
@@ -0,0 +1,9 @@
+/*
+    Chacun des éléments des listes déroulantes est un bouton.
+    Il faut donc générer ces boutons dynamiquement
+    puis les ajouter à la liste des "tags" en cas de clic
+*/
+
+class factoryBoutons {
+    
+}
