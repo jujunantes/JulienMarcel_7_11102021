838f1efe99855083fdca232bfaaec603f997822f
Julien MARCEL <jul_marcel@hotmail.com>
2021-10-27 09:21:54 +0200

    Population listes déroulantes

--- js/factoryBoutons.js
+++ js/factoryBoutons.js
@@ -1,9 +1,9 @@
 /*
-    Chacun des éléments des listes déroulantes est un bouton.
+    Chacun des éléments sélectionné des listes déroulantes est un bouton.
     Il faut donc générer ces boutons dynamiquement
-    puis les ajouter à la liste des "tags" en cas de clic
+    puis les ajouter à la liste des filtres
 */
 
 class factoryBoutons {
-    
+
 }
--- js/index.js
+++ js/index.js
@@ -3,20 +3,57 @@ import { Recette } from './classes.js';
 const fetchRecettes = [];
 const mesRecettes = [];
 
-const IngredientsJSON = [];
-const AppareilsJSON = [];
-const UstensilesJSON = [];
+let IngredientsJSON = [];
+let AppareilsJSON = [];
+let UstensilesJSON = [];
 
 const chargeRecettes = async () => {
-  const affichageRecettes = document.getElementById('affichageRecettes');
+  const affichageRecettes = document.getElementById('affichageRecettes')
   const fetchRecettes = await fetch('js/recettes.json')
     .then((reponse) => reponse.json())
     .then((donnees) => {
       for (const recette of donnees.recipes) {
         const maRecette = new Recette(recette.id, recette.name, recette.servings, recette.ingredients, recette.time, recette.description, recette.appliance, recette.ustensils);
-        affichageRecettes.innerHTML += maRecette.genereCarteRecette();
+        // On en profite pour récupérer les ingrédients, appareils et ustensiles
+        for(const list of maRecette.ingredients){IngredientsJSON.push(list.ingredient)}
+        AppareilsJSON.push(maRecette.appliance) // Il n'y a qu'un seul appareil par recette
+        for(const element of maRecette.ustensils){UstensilesJSON.push(element)}
+        // Et on élimine les doubles
+        IngredientsJSON = Array.from(new Set(IngredientsJSON))
+        AppareilsJSON = Array.from(new Set(AppareilsJSON))
+        UstensilesJSON = Array.from(new Set(UstensilesJSON))
+        // On trie les tableaux
+        IngredientsJSON.sort(function (a, b) {return a.localeCompare(b)}) // Pour tenir compte des caractères accentués
+        AppareilsJSON.sort(function (a, b) {return a.localeCompare(b)})
+        UstensilesJSON.sort(function (a, b) {return a.localeCompare(b)})
+        // On affiche la recette
+        affichageRecettes.innerHTML += maRecette.genereCarteRecette()
       }
     });
+    /*console.log(IngredientsJSON)
+    console.log(AppareilsJSON)
+    console.log(UstensilesJSON)*/
+    // On peut maintenant peupler les listes déroulantes
+    const affichageIngredients = document.getElementById('listeIngredients')
+    IngredientsJSON.forEach((element) => {
+      affichageIngredients.innerHTML += `<li>${element}</li>`;
+    })
+    document.getElementById("conteneur-1_deplie").style.display = "block"
+
+    const affichageAppareils = document.getElementById('listeAppareils')
+    AppareilsJSON.forEach((element) => {
+      affichageAppareils.innerHTML += `<li>${element}</li>`;
+    })
+    document.getElementById("conteneur-2_deplie").style.display = "block"
+
+    const affichageUstensiles = document.getElementById('listeUstensiles')
+    UstensilesJSON.forEach((element) => {
+      affichageUstensiles.innerHTML += `<li>${element}</li>`;
+    })
+    document.getElementById("conteneur-3_deplie").style.display = "block"
 };
+
+// Gestion des liste déroulantes
+
 
 chargeRecettes();
